cee89ae12719207e6708c7cdee1c6cfb7dd1f05c
VasilBorodin <79115303+VasilBorodin@users.noreply.github.com>
2021-07-18 21:28:55 +0300

    Добавил картинки

--- src/app.js
+++ src/app.js
@@ -63,3 +63,25 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+//galleryContainer//
+
+function createMarkupGalleryItem(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  >
+    <img
+      class="gallery__image"
+      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+      alt="Tulips"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
